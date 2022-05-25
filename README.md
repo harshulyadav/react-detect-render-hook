1e995e35ed0999eaadadc87647497023bffb4ebf
harshulyadav <harshulyadav@gmail.com>
2022-05-25 22:57:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-tffceu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-tffceu)
---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import { useDidUpdate } from './useDidUpdate';
+import './style.css';
+
+export default function App() {
+  const [state, setState] = useState({
+    key: false,
+  });
+
+  useEffect(() => {
+    console.log('ran for first time');
+  }, []);
+
+  useDidUpdate(() => {
+    // react please run me if 'key' changes, but not on initial render
+    console.log('Yes updated')
+  }, [state.key]);
+
+  const handleClick = (e) => {
+    console.log('clicked');
+    setState(!state);
+  };
+
+  return (
+    <div onClick={handleClick}>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
